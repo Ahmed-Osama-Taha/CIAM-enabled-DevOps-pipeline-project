74db16572fced8f93a931322507295e1902331c0
Ahmed Taha <ahmed.osama23@msa.edu.eg>
2025-08-13 00:36:39 +0300

    auth

--- src/feedback_frontend/src/AuthService.js
+++ src/feedback_frontend/src/AuthService.js
@@ -4,15 +4,15 @@ import { UserManager, WebStorageStateStore } from 'oidc-client-ts';
 class AuthService {
   constructor() {
     const settings = {
-      authority: 'http://localhost:8080/realms/feedback-realm', // Your Keycloak realm
+      authority: 'http://172.20.10.5:8080/realms/feedback-realm', // Your Keycloak realm
       client_id: 'feedback_frontend',
-      redirect_uri: 'http://localhost:3000/callback',
-      post_logout_redirect_uri: 'http://localhost:3000',
+      redirect_uri: 'http://172.20.10.5:30080/callback',
+      post_logout_redirect_uri: 'http://172.20.10.5:30080',
       response_type: 'code',
       scope: 'openid profile email',
       userStore: new WebStorageStateStore({ store: window.localStorage }),
       automaticSilentRenew: true,
-      silent_redirect_uri: 'http://localhost:3000/silent-callback'
+      silent_redirect_uri: 'http://172.20.10.5:30080/silent-callback'
     };
 
     this.userManager = new UserManager(settings);
